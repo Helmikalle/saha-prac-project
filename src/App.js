7182e851899a7999ba64354edf37b1a2c7bfad2e
Kalle Helminen <kalhelm@hotmail.com>
2019-01-03 10:47:51 +0200

    Nav bar styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header className="Header"/>  
+        <Header />  
         <div className="App">
           <PageLayout />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import '../styles/Navbar.css';
 
 export default class Header extends React.Component {
+  toggleHidden = () => {
+    const navs = document.querySelectorAll('.Navbar__Items')
+  
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+
   render() {
     return(
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#home">Saha</a>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link" href="#home">Home</a>
-          <a className="nav-item nav-link" href="#intro-text">Intro</a>
-          <a className="nav-item nav-link" href="#contact-form">Contact Form</a>
+      <div className="Topnavbar">
+        <div className="Navbar__Link Navbar__Link-brand">
+          <h2 onClick={this.toggleHidden} className="Navbar__header" >Saha</h2> 
+        </div>
+        <nav className="Navbar__Items">
+          <div className="Navbar__Link">
+            <a onClick={this.toggleHidden} href="#home" className="Navbar__item">Home</a>
+          </div>
+          <div className="Navbar__Link">
+            <a onClick={this.toggleHidden} href="#intro-text" className="Navbar__item">Intro</a>
+          </div>
+          <div className="Navbar__Link">
+            <a onClick={this.toggleHidden} href="#contact-form" className="Navbar__item">Contact Form</a>
+          </div>
+        </nav>
+        <div className="Navbar__Link Navbar__Link-toggle" onClick={this.toggleHidden}>
+          LINK
         </div>
       </div>
-    </nav>
     );
   }
 }
